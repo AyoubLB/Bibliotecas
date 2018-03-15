56d47d4a15ba08950d5b73d4c2167c80276085ae
AyoubLB <AyoubLB@users.noreply.github.com>
2018-03-15 16:53:05 +0100

    Test2

--- app/src/androidTest/java/com/example/software/bibliotecas/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/software/bibliotecas/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
+        //test2
         assertEquals("com.example.software.bibliotecas", appContext.getPackageName());
     }
 }
