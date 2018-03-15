5aa6b8623f992f7d2c6a346642010520d7dde0f0
AyoubLB <33597418+AyoubLB@users.noreply.github.com>
2018-03-15 17:02:27 +0100

    Test1 (#1)
    * Test1
    
    * Test2

--- app/src/main/java/com/example/software/bibliotecas/MainActivity.java
+++ app/src/main/java/com/example/software/bibliotecas/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //Test1
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //Test2
     }
 
 }
